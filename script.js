c9dd909cfde1153250ece4c2baa81245a5168511
JaceDomingo <jace208753@student-schoolsofwestfield.org>
2024-11-19 15:00:37 +0000

    done

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var score = [0,0]
+var score = [0,0];
 
 function main(){
     let winner = 3;
-    let rounds = setRounds;
-    while (rounds <= rounds){
-        winner = rpsRound;
+    let rounds = setRounds();
+    let round = 1;
+    while (round <= rounds){
+        winner = rpsRound();
         score[winner]++;
+        round++;
     }
     alert("You have "+score[0]+", and I have "+score[1]);
     if (score[0]>score[1]) alert("You win!");
@@ -13,7 +15,7 @@ function main(){
 }
 
 function setRounds(){
-    rounds = prompt("How many rounds do you want to play? (Must be odd)")
+    let rounds = prompt("How many rounds do you want to play? (Must be odd)")
     if (rounds%2 == 0){
         alert("Must be odd");
         return setRounds();
@@ -31,8 +33,8 @@ function rpsRound(){
             alert("We both chose "+cChoice);
         }
     }
-    winner = findwinner(uChoice,cChoice);
-    players = ["You","I"];
+    winner = findWinner(uChoice,cChoice);
+    players = ["you","I"];
     let win = players.indexOf(winner);
     return win;
 }
@@ -44,4 +46,21 @@ function userTurn(){
 	    return choice;
     }
     else userTurn();
+}
+
+function cpuTurn() {
+    let moves = ["r","p","s"];
+    let cpuTurn = Math.floor(Math.random()*2)
+    return moves[cpuTurn];
+}
+
+function findWinner(u,c){    
+    let winArray=[["r","p","I"],["r","s","you"],["p","s","I"],["p","r","you"],["s","r","I"],["s","p","you"]];
+    for (let i = 0; i < winArray.length; i++){
+          if (winArray[i][0] == u && winArray[i][1] == c){ 
+            winner = winArray[i][2];
+         }
+    }
+    alert("I chose "+c+", and you chose "+u+". "+winner+" win!");
+  return winner;
 }
